c0e7c55188ffe4ec5af7c041102dbc8e2747d515
Edmund Nopuente <enopuente@gmail.com>
2019-08-21 14:15:52 -0400

    updated launch config for staging spot price

--- create-launch-configuration.sh
+++ create-launch-configuration.sh
@@ -1,8 +1,14 @@
 printf "create launch configuration ($launch_configuration_name)"
-launch_configuration=`aws autoscaling create-launch-configuration \
+launch_configuration="aws autoscaling create-launch-configuration \
   --launch-configuration-name $launch_configuration_name \
   --image-id $image_id \
   --instance-type $instance_type \
   --associate-public-ip-address \
-  --security-groups $security_group_ids`
+  --security-groups $security_group_ids"
+
+  if [ $environment_tier != "production" ]; then
+    launch_configuration="$launch_configuration --spot-price 0.044"
+  fi
+
+ launch_configuration=`$launch_configuration`
 echo " . done"
