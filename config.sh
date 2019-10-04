4e7c3a48b0dd6dc73595ac647a5b923c07d927b1
Edmund Nopuente <enopuente@gmail.com>
2019-10-04 13:41:27 -0400

    updated config to 12 instances for prod

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@ instance_id=$1
 user_id=$2
 domain=teluswebteam.com
 aws_regions=(us-west-2 us-west-1 us-east-1)
-autoscaling_group_desired_capacity_production=6
-autoscaling_group_desired_capacity_staging=3
+autoscaling_group_desired_capacity_production=12
+autoscaling_group_desired_capacity_staging=1
