cd3507cf25af1fe8ebfb8e2cf63305a583f3d1f9
Edmund Nopuente <enopuente@gmail.com>
2019-10-08 15:10:08 -0400

    update staging app capacity

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ user_id=$2
 domain=teluswebteam.com
 aws_regions=(us-west-2 us-west-1 us-east-1)
 autoscaling_group_desired_capacity_production=12
-autoscaling_group_desired_capacity_staging=1
+autoscaling_group_desired_capacity_staging=3
