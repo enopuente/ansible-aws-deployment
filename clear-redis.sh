a3f35e45dd42d2a267da1e97f844ed7cafd9c2ac
Edmund Nopuente <edmund.nopuente@telus.com>
2019-12-10 01:11:47 -0500

    updated clear redis

--- clear-redis.sh
+++ clear-redis.sh
@@ -1,9 +1,3 @@
 printf "Clearing redis cached languages in $environment_tier..."
-if [ $environment_tier == "production" ]; then
-  redis_host="koodo-ss-02-prod.1jit0g.0001.use1.cache.amazonaws.com"
-else
-  redis_host="koodo-ss-02-stag.df4rwc.0001.usw2.cache.amazonaws.com"
-fi
-
-ssh -t $user_id@management.$project-$environment_tier.$domain ssh $role-direct.$project-$environment_tier.$domain "redis-cli -h $redis_host --scan --pattern \"koodo-selfserve*\" | xargs redis-cli -h $redis_host del"
+ssh -t $user_id@management.$project-$environment_tier.$domain ssh $role-direct.$project-$environment_tier.$domain "bash /data/scripts/clear-redis.sh"
 echo "Done"
